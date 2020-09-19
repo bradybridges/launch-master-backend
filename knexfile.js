96845941352c0c63a309eccf4b08ea01fe6e6412
Brady Bridges <bradyjbridges@gmail.com>
2020-09-19 14:25:45 -0500

    Remove ssl true for production database config, trying to fix self signed cert error

--- knexfile.js
+++ knexfile.js
@@ -24,7 +24,7 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL + `?ssl=true`,
+    connection: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false },
     useNullAsDefault: true,
     migrations: {
